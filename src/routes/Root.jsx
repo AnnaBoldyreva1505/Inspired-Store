09f39e85b4f21700385e541fcbffc9f4713fb419
Anna_Boldyreva <hannabl1988@gmail.com>
2023-09-19 00:38:27 +0600

    add scroll

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../Components/Header/Header.jsx";
 import { Main } from "../Components/Layout/Main/Main.jsx";
 import { Footer } from "../Components/Footer/Footer.jsx";
-
-
+import ScrollToTop from "react-scroll-up";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export const Root = () => (
   <>
-
+    <ScrollToTop showUnder={160}>
+      <span>
+        <BsFillArrowUpCircleFill fill="#000000" size={40} />
+      </span>
+    </ScrollToTop>
     <Header />
     <Main>
       <Outlet />
