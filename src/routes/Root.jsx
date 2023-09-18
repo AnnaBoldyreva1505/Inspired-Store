7281d8d328188804ddb2ca69d8a2e364fefefb00
Anna_Boldyreva <hannabl1988@gmail.com>
2023-09-19 00:31:59 +0600

    +

---
+++ src/routes/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "../Components/Header/Header.jsx";
+import { Main } from "../Components/Layout/Main/Main.jsx";
+import { Footer } from "../Components/Footer/Footer.jsx";
+
+
+
+export const Root = () => (
+  <>
+
+    <Header />
+    <Main>
+      <Outlet />
+    </Main>
+    <Footer />
+  </>
+);
